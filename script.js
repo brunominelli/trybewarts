d8057b3871b8e9f8498eeede33a6c1b97fed6286
Bruno Minelli <minellibruno@hotmail.com>
2022-01-11 18:29:50 -0300

    Finalizaçao do requisito 03

--- script.js
+++ script.js
@@ -4,13 +4,9 @@ const buttonLogin = document.getElementById('login');
 console.log(email.value);
 console.log(password.value);
 
-buttonLogin.addEventListener('click', () => {
-  if (email.value === 'tryber@teste.com' && password.value === '123456') {
-    alert('Olá, Tryber!');
-  }
-});
-
-/* if (email.value === 'tryber@teste.com' && password.value === '123456') {
-  alert('Deu boa');
+function validateLogin() {
+  if (email.value === 'tryber@teste.com' && password.value === '123456')alert('Olá, Tryber!');
+  else alert('Email ou senha inválidos.');
 }
-*/
+
+buttonLogin.addEventListener('click', validateLogin);
