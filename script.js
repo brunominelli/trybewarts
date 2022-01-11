fe508cd54d12fc2d53c9bf83219ab560d717bf3b
Bruno Minelli <minellibruno@hotmail.com>
2022-01-11 18:43:04 -0300

    Requisito 09 finalizado

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
+/* Login */
 const email = document.getElementsByName('email')[0];
 const password = document.getElementsByName('password')[0];
 const buttonLogin = document.getElementById('login');
-console.log(email.value);
-console.log(password.value);
+
+/* Select */
+const select = document.getElementById('house');
+const arrayHouse = ['Gitnória', 'Reactpuff', 'Corvinode', 'Pytherina'];
+const arrayHouseId = ['gitnoria-house', 'reactpuff-house', 'corvinode-house', 'pytherina-house'];
+
+function setHouses(array1, array2) {
+  for (let index = 0; index < array1.length; index += 1) {
+    const option = document.createElement('option');
+    option.value = array1[index];
+    option.innerText = array1[index];
+    option.id = array2[index];
+    select.appendChild(option);
+  }
+}
 
 function validateLogin() {
   if (email.value === 'tryber@teste.com' && password.value === '123456')alert('Olá, Tryber!');
@@ -10,3 +24,7 @@ function validateLogin() {
 }
 
 buttonLogin.addEventListener('click', validateLogin);
+
+window.onload = () => {
+  setHouses(arrayHouse, arrayHouseId);
+};
